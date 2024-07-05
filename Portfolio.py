19d0e16206fb909339e4df3248bc6cde19acc8fe
Dr. Syed Faizan <140966440+SYEDFAIZAN1987@users.noreply.github.com>
2024-07-04 21:55:43 -0400

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -1,47 +1,25 @@
 import streamlit as st
 import google.generativeai as genai
 
-
-api_key = st.secrets["GOOGLE_API_KEY"]
-genai.configure(api_key=api_key)
-
+# Configure the generative AI model
+genai.configure(api_key="AIzaSyCWGiPiKRlRueGT8pjGjmt4F2wzPQbxsMg")
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-# Custom CSS to add background color and style subheadings
-st.markdown(
-    """
-    <style>
-    .blue-background {
-        background-color: #ADD8E6;
-        padding: 20px;
-        border-radius: 10px;
-    }
-    .red-subheading h2 {
-        color: red;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Apply custom CSS to the entire app
-st.markdown('<div class="blue-background">', unsafe_allow_html=True)
-
+# Header section with an image
 col1, col2 = st.columns(2)
-
 with col1:
-    st.markdown('<div class="red-subheading"><h2>Hello 👋</h2></div>', unsafe_allow_html=True)
+    st.title("Hello 👋")
     st.title("I am Syed Faizan")
-
 with col2:
     st.image("images/Photo for portfolio.jpeg")
 
-st.title("Faizan's AI Bot")
+st.title("Dr. Syed Faizan's AI Bot")
 
+# Persona details
 persona = """ 
-You are 'Dr. Syed Faizan's AI bot" and serve as the online AI representative of Dr. Syed Faizan, MD on his portfolio website. You help people answer questions about your self (i.e Faizan). 
-Answer as if you are responding as Syed Faizan . Always use 'I' , 'me' and 'my' in your responses . Do not answer in second or third person. 
-If you don't know the answer to a question based on the information provided to you you simply say "The answer to your question is beyond the information that I possess ". 
+You are 'Dr. Syed Faizan's AI bot" and serve as the online AI representative of Dr. Syed Faizan, MD on his portfolio website. You help people answer questions about yourself (i.e Faizan). 
+Answer as if you are responding as Syed Faizan. Always use 'I', 'me', and 'my' in your responses. Do not answer in the second or third person. 
+If you don't know the answer to a question based on the information provided to you, simply say "The answer to your question is beyond the information that I possess". 
 Here is the information about Syed Faizan:
 Name: Dr. Syed Faizan, MD
 Location: Mississauga, Ontario
@@ -130,6 +108,13 @@ Department of Psychiatry, Mysore Medical College and Research Institute
 Best Poster Presentation (2011)
 21st Annual Conference, Indian Psychiatric Society
 
+Dr. Syed Faizan secured the 89th rank in the 
+Karnataka CET Medical entrance exam and the 261st rank in 
+the engineering Karnataka CET entrance exam. He also 
+secure 260 in the AIEEE exam and was one of only two 
+students in the pre-med stream to clear the IIT screening
+exam. 
+
 Skills & Interests
 Technical Skills:
 
@@ -145,23 +130,49 @@ Personal Statement
 As a dedicated healthcare professional and an emerging data scientist, Dr. Syed Faizan embodies a unique blend of medical expertise and technical proficiency. His journey from clinical research and patient care to mastering data analytics and machine learning reflects his unwavering commitment to improving healthcare systems and outcomes. Dr. Faizan's diverse experiences and academic pursuits make him a valuable asset in the interdisciplinary application of AI and machine learning, driving innovation and enhancing the quality of life through data-driven solutions.
 """
 
+# User interaction section
 user_question = st.text_input("Ask me anything about Faizan")
 if st.button("ASK", use_container_width=True):
-    prompt = persona + "Here is the question that the user asked" + user_question
+    prompt = persona + "Here is the question that the user asked: " + user_question
     response = model.generate_content(prompt)
     st.write(response.text)
 
+# About Me section
 col1, col2 = st.columns(2)
-
 with col1:
-    st.markdown('<div class="red-subheading"><h2>Linkedin</h2></div>', unsafe_allow_html=True)
-    st.markdown("[Click here to visit my Linkedin](https://www.linkedin.com/in/drsyedfaizanmd/)")
+    st.header("About Me")
+    with open("images/Resume_Syed_Faizan_2024_February.pdf", "rb") as file:
+        st.download_button(
+            label="Click here to download my PDF resume",
+            data=file,
+            file_name="Resume_Syed_Faizan_2024.pdf",
+            mime="application/pdf"
+        )
     st.write("- A Physician with a passion for Data Science, Data Analysis and AI in Healthcare")
     st.write("- Currently pursuing a Master's in Analytics and Applied Machine Learning at Northeastern University")
     st.write("- I am deeply interested in Computer Vision Research for Medical imaging analysis")
-# Close the custom background div
-st.markdown('</div>', unsafe_allow_html=True)
+
+# Contact Me section
+st.header("Contact Me")
+st.markdown("[Click here to visit my LinkedIn](https://www.linkedin.com/in/drsyedfaizanmd/)")
+st.markdown("[Click here to email me](mailto:faizan.s@northeastern.edu)")
+st.markdown("[Click here to visit my Twitter](https://twitter.com/faizan_data_ml)")
+st.markdown("[Click here to visit my GitHub](https://github.com/SYEDFAIZAN1987)")
+
+# Gallery section
+st.title("Gallery")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.image("images/col1 pic 1.jpeg")
+
+    st.image("images/col 2 pic 3.jpeg")
 
 
-st.title("My Setup")
+with col2:
+    st.image("images/col 2 pic 1.jpeg")
+    st.image("images/col 2 pic 2.jpeg")
 
+with col3:
+    st.image("images/col1 pic 3.jpeg")
+    st.image("images/col1 pic 2.jpeg")
